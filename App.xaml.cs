42537d4a19f55b60345d97bac0297f0fb9e8bdfb
dracken24 <dracken24@gmail.com>
2025-08-12 14:46:48 -0400

    init

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TP_2_Developpement_Application_Burreau
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
